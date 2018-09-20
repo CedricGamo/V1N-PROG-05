0b9111ce7cbadfeb6993c3bc04d0e79b345acc84
cedricluvumbo <cedric.luvumbo@student.hu.nl>
2018-09-20 09:06:10 +0200

    Opdrachten

---
+++ LES-05/Oefening 5_1.py
@@ -0,0 +1,11 @@
+naam = (input ('Wat is je naam?: '))
+leeftijd = int (input ('Wat is je leeftijd?: '))
+
+if (leeftijd) > 17:
+    print (naam + ', Je mag stemmen! Gefeliciteerd!')
+
+else :
+    print (naam + ', Je mag niet stemmen!')
+
+
+
---
+++ LES-05/Oefening 5_2.py
@@ -0,0 +1,10 @@
+woordenlijst = ['aap', 'noot', 'hond']
+
+for woord in woordenlijst:
+    print (woord)
+
+woord = 'voorbeeld'
+
+for letter in woord:
+    print (letter)
+
